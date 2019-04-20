1748131203993decec1812f9bbb3475d591732c0
IceSword8492 <cleared.for.approach.runway23@gmail.com>
2019-04-20 13:21:44 +0900

    added js folder

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let url = {
+  getParams: () => {
+    let params = {};
+    let param = location.search.substring(1).split("&");
+    for (let i = 0; i < param.length; i++)
+    {
+      let keySearch = param[i].search(/=/);
+      let key = "";
+      if (keySearch != -1)
+      {
+        key = param[i].slice(0, keySearch);
+      }
+      let val = param[i].slice(param[i].indexOf("=", 0) + 1);
+      if (key != "")
+      {
+        params[key] = decodeURI(val);
+      }
+    }
+    return params;
+  },
+  params: utl.getParams()
+};
+
+console.log(url.params);
